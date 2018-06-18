9014cd57350bdb4bff0d4db084ae5fc360dab2f7
ktpolanski <krzysztof_polanski@o2.pl>
2018-06-18 22:02:42 +0100

    pip structure!

---
+++ python_module/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='bbknn',
+	version='1.0.0',
+	description='Batch balanced KNN',
+	url='https://github.com/Teichlab/bbknn',
+	packages=find_packages(),
+	install_requires=['Cython','numpy','scanpy'],
+	author='Krzysztof Polanski, Jongeun Park',
+	author_email='kp9@sanger.ac.uk',
+	license='MIT'
+)
