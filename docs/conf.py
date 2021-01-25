6e32a707c0df7806ce81807087abb44d762ae7cc
ktpolanski <krzysztof_polanski@o2.pl>
2021-01-25 11:56:43 +0000

    update year and try removing newer sphinx from requirements as no mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'BBKNN'
-copyright = '2018-2020, Krzysztof Polanski, Jongeun Park'
+copyright = '2018-2021, Krzysztof Polanski, Jongeun Park'
 author = 'Krzysztof Polanski, Jongeun Park'
 
 # The short X.Y version
